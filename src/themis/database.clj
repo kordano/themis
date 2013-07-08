f642a279e98bcf75d18116cee51f08d6869ec21f
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-08 14:45:08 +0200

    some database experiments

--- src/themis/database.clj
+++ src/themis/database.clj
@@ -6,5 +6,7 @@
     (get-database "projects")))
 
 
-(bulk-update "projects" [{:name "ceres" :_id "0001"}
+(bulk-update "projects" [{:name "ceres" :test {:sub "12" :sub2 "13"} :_id "0001"}
                          {:name "acheron" :_id "0002"}])
+
+(get-document "projects" "0001")
