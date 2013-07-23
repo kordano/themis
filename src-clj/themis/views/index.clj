058c06d5ab2cd70ee02fd85ddb68c2774ac30492
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-23 17:57:40 +0200

    some minor layout changes

--- src-clj/themis/views/index.clj
+++ src-clj/themis/views/index.clj
@@ -1,86 +1,9 @@
-(ns themis.server.views
+(ns themis.views.index
   (:use themis.database
-        [garden.core :only [css]]
-        [garden.units :only [pt px em]]
-        [garden.color :as color]
+        themis.views.css
         [hiccup.core :only [html]]
         [hiccup.page :only [html5 include-js]]
         [hiccup.element :only [javascript-tag]]))
-
-
-(def header-height (px 150))
-(def default-font "Open Sans")
-(def header-font-size (pt 12))
-(def default-font-color "#FFFFFF")
-(def hover-color "#F27E18")
-(def default-background-color "#242424")
-
-(defn body-css []
-  [:body :html
-   {:font-family default-font
-    :font-size header-font-size
-    :margin 0}])
-
-
-(defn nav-css []
-  [:#projectnav
-   {:margin 0
-    :padding [0 (em 1)]
-    :background default-background-color
-    :height (em 3)
-    :text-decoration :none
-    :list-style :none}
-   ["& > li"
-    {:float :left
-     :margin-right (em 0.5)
-     :padding [0 (em 1)]}
-    [:& :a
-     {:float :left
-      :color default-font-color
-      :text-decoration :none
-      :line-height 3}
-     [:&:hover
-      {:color hover-color
-       :text-decoration :none}]]]])
-
-
-(defn header-logo-css []
-  [:#header-description
-   {:float :left
-    :margin-right (em 0.5)
-    :padding [0 (em 1)]}
-   [:& :a
-    {:float :left
-     :color hover-color
-     :text-decoration :none
-     :line-height 3
-     :font-weight :bold}
-    [:&:hover
-     {:color hover-color
-      :text-decoration :none}]]])
-
-(defn task-container-css []
-  [:#task-container
-   {:float :left
-    :margin 0
-    :padding-top (em 1.5)
-    :padding-left (em 1.5)}
-   ["& > a"
-    {:text-align :center
-     :font-weight :bold}]])
-
-(defn tasks-css []
-  [:.task
-   {:float :left
-    :color (rgb 113 39 131)}])
-
-(defn overall-css []
-  (css
-   (body-css)
-   (nav-css)
-   (header-logo-css)
-   (task-container-css)
-   (tasks-css)))
 
 
 (defn projects-list []
@@ -105,8 +28,7 @@
       [:div#task-container
        [:a "Tasks"]
        [:ul
-        [:li.task [:a "test"]]]]]]]
-   ))
+        [:li.task [:a "test"]]]]]]]))
 
 
 #_(run-clojurescript "js/cljs.js" "themis.repl.connect()")
