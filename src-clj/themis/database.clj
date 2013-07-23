e1db736a6091cd02d0e9608897209813ab7cacd2
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-23 17:59:29 +0200

    consider defprotocols

--- src-clj/themis/database.clj
+++ src-clj/themis/database.clj
@@ -1,15 +1,38 @@
 (ns themis.database
   (:refer-clojure :exclude [assoc! conj! dissoc!])
   (:require [clojure.core :as core])
-  (:use
-   [com.ashafa.clutch]))
+  (:use themis.structures
+        [com.ashafa.clutch])
+  (:import [themis.structures Task Project User Person Contact Address]))
 
 #_(defn init-db []
   (do
     (get-database "projects")
     (get-database "tasks")
+    (get-database "users")
     (get-database "people")))
 
 
+(defprotocol database-interaction
+  (inject [entry]))
+
+
 (defn get-all-projects []
-  (map #((get-document "projects" (% :key)) :name) (all-documents "projects")))
+  )
+
+
+
+(inject
+  (create-person
+   :first-name "Konrad"
+   :surname "Kühne"
+   :birthday "15.04.1985"
+   :contact (create-contact
+             :email "konnykuehne@googlemail.com"
+             :address (create-address
+                       :zipcode "69151"
+                       :city "Neckargemünd"
+                       :street "Kurt-Lindemannstrasse 5")
+             :phone "0622372661")))
+
+(inject (create-user "kordano"))
