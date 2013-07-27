01230ef17732142761e43bbb6754d4f8ad632dd9
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-27 21:53:17 +0200

    remove old api and replace with shiny new api

--- src-clj/themis/core.clj
+++ src-clj/themis/core.clj
@@ -1,49 +1,26 @@
-(ns themis.core
-  (:use compojure.core
-        themis.views.index
-        cheshire.core
-        ring.util.response
-        [hiccup.middleware :only (wrap-base-url)])
-  (:require [themis.database :as db]
-            [compojure.route :as route]
-            [compojure.handler :as handler]
-            [compojure.response :as response]
-            [ring.middleware.json :as middleware]))
+(ns themis.tricore
+  (:use ring.adapter.jetty
+        ring.middleware.file
+        ring.middleware.file-info
+        ring.middleware.format-response
+        [ring.util.response :exclude (not-found)]
+        compojure.core
+        compojure.route
+        [themis.database :as db]
+        themis.views.index))
 
+(defroutes handler
+  (GET "/" [] (response (index-page)))
+  (GET "/projects" [] (response (apply vector (map #(:_id %) (db/get-all-documents "projects")))))
+  (GET "/resources/:id" [id] (file-response "main.js" {:root "resources/public"}))
+  (not-found "<h1>404 Page not found</h1>")
+)
 
-(defn project-context []
-    (context "/projects" []
-             (defroutes documents-routes
-               (GET "/" [] (response (generate-string (db/get-all-documents "projects") {:escape-non-ascii true})))
-               (context "/:id" [id] (defroutes document-routes (GET "/" [] (response (generate-string (db/find-project id) {:escape-non-ascii true}))))))))
+(def app (-> handler
+             wrap-clojure-response))
 
+#_(def server
+    (run-jetty #'app {:port 3000 :join? false}))
 
-
-(defn user-context []
-  (context "/users" []
-           (defroutes documents-routes
-             (GET "/" [] (response (db/get-all-documents "themis-users")))
-             (context "/:id" [id] (defroutes document-routes (GET "/" [] (response (generate-string (db/find-user id) {:escape-non-ascii true}))))))))
-
-
-(defroutes main-routes
-  (GET "/" [] (index-page))
-  (GET "/test" [] (generate-string (db/get-all-documents "projects")))
-  (project-context)
-  (user-context)
-  (route/resources "/")
-  (route/not-found "These are not the droids you are looking for."))
-
-(def app
-  (-> (handler/api main-routes)
-      (middleware/wrap-json-body)
-      (middleware/wrap-json-response)))
-
-
-; run these three to start a server on the repl
-#_(use 'ring.adapter.jetty)
-#_(def server (run-jetty #'app {:port 3000 :join? false}))
-
-; control the server
-#_(.stop server)
-#_(.start server)
+#_ (.stop server)
+#_ (.start server)
