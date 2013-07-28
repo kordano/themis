aafbf568132cc4b2fde52a1682db326ba2b58f7b
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-28 23:16:20 +0200

    adjust insert user post route

--- src-clj/themis/core.clj
+++ src-clj/themis/core.clj
@@ -3,31 +3,35 @@
         ring.middleware.file
         ring.middleware.file-info
         ring.middleware.format-response
-        [ring.util.response :exclude (not-found)]
         [clojure.edn :as edn]
+        [ring.util.response :exclude (not-found)]
         compojure.core
         compojure.route
         [themis.database :as db]
         themis.views.index))
 
 
+(defn add-user [name project]
+    (response (db/insert-user name :project project)))
+
 (defroutes handler
   (GET "/" [] (response (index-page)))
   (GET "/projects" [] (response (db/get-all-documents "projects")))
   (GET "/projects/:id" [id] (response (db/find-project id)))
   (GET "/users" [] (response (db/get-all-documents "themis-users")))
-  (POST "/insert/users/" request {:status 200 :body (-> request :body)})
+  (POST "/insert/users/" request (let [data (edn/read-string (slurp (:body request)))]
+                                     (add-user (:name data) (:project data))))
   (files ""  {:root "resources/public"})
   (not-found "<h1>404 Page not found</h1>"))
 
 
-(def app (-> handler
-             wrap-clojure-response))
+(def app
+  (-> handler
+      wrap-clojure-response))
 
 
+#_(.stop server)
 #_(def server
     (run-jetty #'app {:port 3000 :join? false}))
 
-#_(do
-  (.stop server)
-  (.start server))
+#_(.start server)
