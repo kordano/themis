2d7f7176d65734accb89babdee2132d9ae6241b8
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-28 17:19:44 +0200

    add

--- src-clj/themis/core.clj
+++ src-clj/themis/core.clj
@@ -4,6 +4,7 @@
         ring.middleware.file-info
         ring.middleware.format-response
         [ring.util.response :exclude (not-found)]
+        [clojure.edn :as edn]
         compojure.core
         compojure.route
         [themis.database :as db]
@@ -15,6 +16,7 @@
   (GET "/projects" [] (response (db/get-all-documents "projects")))
   (GET "/projects/:id" [id] (response (db/find-project id)))
   (GET "/users" [] (response (db/get-all-documents "themis-users")))
+  (POST "/insert/users/" request {:status 200 :body (-> request :body)})
   (files ""  {:root "resources/public"})
   (not-found "<h1>404 Page not found</h1>"))
 
@@ -22,8 +24,8 @@
 (def app (-> handler
              wrap-clojure-response))
 
-#_(def server
-    (run-jetty #'app {:port 3000 :join? false}))
-
 #_ (.stop server)
 #_ (.start server)
+
+#_(def server
+    (run-jetty #'app {:port 3000 :join? false}))
