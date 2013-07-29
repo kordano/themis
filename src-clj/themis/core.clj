e7419c96269754f9375f7a98f8d750080e72a9fe
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-07-29 23:06:44 +0200

    adjust routes and functions to member-record

--- src-clj/themis/core.clj
+++ src-clj/themis/core.clj
@@ -1,26 +1,33 @@
 (ns themis.core
+  (refer-clojure :exlude [read-string])
   (:use ring.adapter.jetty
         ring.middleware.file
         ring.middleware.file-info
         ring.middleware.format-response
-        [clojure.edn :as edn]
         [ring.util.response :exclude (not-found)]
         compojure.core
         compojure.route
         [themis.database :as db]
-        themis.views.index))
+        themis.views.index)
+  (:require [clojure.edn :as edn :refer [read-string]]))
 
 
-(defn add-user [name project]
-    (response (db/insert-user name :project project)))
+(defn add-task [name project]
+  (response (db/insert-task name :project project)))
+
+(defn add-member [name project]
+    (response (db/insert-member name :project project)))
 
 (defroutes handler
   (GET "/" [] (response (index-page)))
   (GET "/projects" [] (response (db/get-all-documents "projects")))
   (GET "/projects/:id" [id] (response (db/find-project id)))
   (GET "/users" [] (response (db/get-all-documents "themis-users")))
-  (POST "/insert/users/" request (let [data (edn/read-string (slurp (:body request)))]
-                                     (add-user (:name data) (:project data))))
+  (POST "/insert/member/" request (let [data (read-string (slurp (:body request)))]
+                                   (add-member (:name data) (:project data))))
+  (POST "/insert/task/" request (let [data (read-string (slurp (:body request)))]
+                                   (add-task (:name data) (:project data))))
+  (GET "/tasks" [] (response ))
   (files ""  {:root "resources/public"})
   (not-found "<h1>404 Page not found</h1>"))
 
