fe2d8f4f1c07cf46c14987eff67b762d0984bade
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-08-01 23:28:35 +0200

    add taskbar div

--- src-clj/themis/views/index.clj
+++ src-clj/themis/views/index.clj
@@ -3,7 +3,9 @@
         themis.views.css
         [hiccup.core :only [html]]
         [hiccup.page :only [html5 include-js]]
-        [hiccup.element :only [javascript-tag]]))
+        [hiccup.element :only [javascript-tag]])
+  (:import [themis.structures Task Project Member Person Contact Address]))
+
 
 (defn- run-clojurescript [path init]
   (list
@@ -14,8 +16,16 @@
   [:div {:id (str type "-window") :class "type-window"}
        [:a title] [:button.add-button {:type "button" :id (str type "-add-button")} "Add"]
    [:div {:id (str type "-list-window")}
-        [:ul {:id (str type "-list") :class "type-list"}]]])
+    [:ul {:id (str type "-list") :class "type-list"}]]])
 
+
+(defn action-bar []
+  [:div#action-bar.bar
+   [:ul
+    [:li [:a "&#9773;"]]
+    [:li [:a "&#8853;"]]
+    [:li [:a "&#8854;"]]
+    [:li [:a "&#9760;"]]]])
 
 (defn index-page []
   (html5
@@ -29,11 +39,9 @@
       [:div#header-description
        [:a#projects "Projects"]]
       [:div#header-projects-list
-       [:ul#projectnav]
-       [:button#timer-button {:type "button"} "TIMER"]]]
+       [:ul#projectnav]]]
+     (action-bar)
      [:div#container
       (list-window "task" "Tasks")
-      (list-window "member" "Members")
-      ]]
+      (list-window "member" "Members")]]
     (run-clojurescript "main.js" "themis.repl.connect()")]))
-;
