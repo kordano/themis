c0f0fb4145c1ec633c04ec7560fd9ae413a74f82
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-08-06 19:20:07 +0200

    create submit window on click

--- src-cljs/themis/client.cljs
+++ src-cljs/themis/client.cljs
@@ -4,10 +4,11 @@
             [goog.net.XhrIo :as xhr]
             [cljs.reader :refer [read-string]]
             [themis.communicator :refer [get-edn post-edn]]
+            [themis.windows :refer [create-addition-window]]
             [cljs.core.async :as async :refer [chan close! put!]])
   (:require-macros [cljs.core.async.macros :refer [go alt!]]
                    [hiccups.core :as hiccups]
-                   [dommy.macros :refer [sel sel1]]))
+                   [dommy.macros :refer [sel sel1 node]]))
 
 
 ; fire up a repl for the browser and eval namespace on top once connected
@@ -21,11 +22,16 @@
 (def state
   (atom {:active-project nil
          :project-data nil
-         :container-state nil}))
+         :available-projects nil
+         :available-types nil}))
 
 
 (defn log [s]
   (.log js/console (str s)))
+
+
+(defn type-keys [specific-type]
+  (specific-type (:available-types (deref state))))
 
 
 (defn create-task-list [data]
@@ -72,10 +78,12 @@
 (defn show-all-projects []
   (go
    (let [data (<! (get-edn "projects"))
-         names (map #(:_id %) data)
+         names (:current-projects data)
+         types (:available-types data)
          html-list (hiccups/html (map #(vector :li [:a.project {:id %} %]) names))]
      (-> (sel1 :#projectnav)
          (dom/set-html! html-list))
+     (swap! state assoc :available-projects names :available-types types)
      (log (map #(set-onclick-project %) names)))))
 
 
@@ -115,7 +123,11 @@
 (defn init []
   (do
     (set! (.-onclick (sel1 :#projects)) (fn [] (show-all-projects)))
-    (set! (.-onclick (sel1 :#task-submit-button)) (fn [] (send-task-data)))))
+    (set! (.-onclick (sel1 :#type-add-action))
+          (fn [] (do (-> (sel1 :#creation-container) (dom/set-html!
+                    (hiccups/html
+                     (create-addition-window "task" "Create new task" type-keys))))
+                    (set! (.-onclick (sel1 :#task-submit-button)) (fn [] (send-task-data))))))))
 
 
 (set! (.-onload js/window) init)
