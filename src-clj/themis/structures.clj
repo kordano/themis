54a2d749c8754db8ea82911787b2151ed31e4f32
kordano <konrad.kuehne@rocketmail.com>
2013-08-18 23:02:36 +0200

    add relation type, adjust other types

--- src-clj/themis/structures.clj
+++ src-clj/themis/structures.clj
@@ -9,12 +9,18 @@
 (defprotocol database-interaction
   (inject [entry]))
 
-(defrecord Project [_id members tasks created-at]
+
+(defrecord Relation [record-id1 record-id2]
+  database-interaction
+  (inject [entry] (put-document "relations" entry)))
+
+
+(defrecord Project [_id created-at]
   database-interaction
   (inject [entry] (put-document "projects" entry)))
 
 
-(defrecord Task [description assigned-to deadline working-time created-at]
+(defrecord Task [description created-at]
   database-interaction
   (inject [entry] (put-document "tasks" entry)))
 
@@ -35,31 +41,30 @@
 (defrecord Address [zipcode city street])
 
 
-(defn create-project
-  [name & {:keys [members tasks created-at] :or {created-at (now)}}]
- (Project. name members tasks created-at))
+(defn create-relation
+  [record-id1 record-id2]
+  (Relation. record-id1 record-id2))
 
 
-(defn create-task
-  [description & {:keys [assigned-to deadline working-time created-at] :or {created-at (now)}}]
-  (Task. description assigned-to deadline working-time created-at))
+(defn create-project [name]
+  (Project. name (now)))
 
 
-(defn create-member
-  [name & person]
+(defn create-task [description]
+  (Task. description (now)))
+
+
+(defn create-member [name & person]
   (Member. name person))
 
 
-(defn create-person
-  [& {:keys [first-name surname birthday contact]}]
+(defn create-person [& {:keys [first-name surname birthday contact]}]
   (Person. first-name surname birthday contact))
 
 
-(defn create-contact
-  [& {:keys [email address phone]}]
+(defn create-contact [& {:keys [email address phone]}]
   (Contact. email address phone))
 
 
-(defn create-address
-  [& {:keys [zipcode city street]}]
+(defn create-address [& {:keys [zipcode city street]}]
   (Address. zipcode city street))
